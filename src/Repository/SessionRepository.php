38e054dcf3db43c540adae2ff09c3ed7af8a0559
Diego <diego.r.izarrualde@gmail.com>
2019-12-16 12:07:42 -0300

    features(SessionService): add methods fetchCommissionsBetweenDates, fetchDealerTipsBetweenDates, fetchServiceTipsBetweenDates, fetchExpensesBetweenDates, fetchTotalCashinBySession

--- src/Repository/SessionRepository.php
+++ src/Repository/SessionRepository.php
@@ -2,28 +2,113 @@
 
 namespace Solcre\Pokerclub\Repository;
 
+use Solcre\Pokerclub\Entity\UserSessionEntity;
 use Solcre\SolcreFramework2\Common\BaseRepository;
 
 class SessionRepository extends BaseRepository
 {
     public function fetchMySessions($username, int $count): array
     {
-        // var_dump( $this->_entityName);
-        var_dump('en sessionRepository'); die();
-        /*var_dump($this->_entityName);
         $qb = $this->_em->createQueryBuilder();
-        $qb->select('*');
+        $qb->select('us.id');
+        $qb->addSelect('us.start');
+        $qb->addSelect('us.end');
+        $qb->addSelect('us.cashout');
+        $qb->addSelect('us.accumulatedPoints');
+        $qb->addSelect('s.id');
 
-        // select all usersessions where el id de usuario esta vinculado a ese username
+        $qb->from(UserSessionEntity::class, 'us');
+        $qb->join('us.user', 'u');
+        $qb->join('us.session', 's');
 
-        $qb->from('UserSessionEntity'/* de session_users *//*, 'us');*/ // $this->_entityName ver que arroja sino
-/*
-        $qb->where('us.user_id = :userId'); /* quizas no sea user_id sino idUser como en entity */
-       /* $qb->andWhere('su.isApproved = 1');
-        $qb->andWhere('s.endTime IS NOT NULL');
-        $qb->orderBy('us.startTime', 'DESC');
+        $qb->where('u.username = :username');
         $qb->setMaxResults($count);
-        $qb->setParameter('userId', $userId);
-        return $qb->getQuery()->getResult();*/
+        $qb->setParameter('username', $username);
+
+        return $qb->getQuery()->getResult();
+    }
+
+    public function fetchCommissionsBetweenDates(\DateTime $from, \DateTime $to)
+    {
+        $qb = $this->_em->createQueryBuilder();
+        $qb->select('s.id');
+        $qb->addSelect('s.startTime');
+        $qb->addSelect('sum(c.commission)');
+        $qb->from('Solcre\Pokerclub\Entity\CommissionSessionEntity', 'c');
+        $qb->join('c.session', 's');
+        $qb->groupBy('s.id');
+        $qb->where('s.startTime BETWEEN :from AND :to');
+        $qb->setParameter('from', $from);
+        $qb->setParameter('to', $to);
+
+        return $qb->getQuery()->getResult();
+    }
+
+    public function fetchDealerTipsBetweenDates(\DateTime $from, \DateTime $to)
+    {
+        $qb = $this->_em->createQueryBuilder();
+        $qb->select('s.id');
+        $qb->addSelect('s.startTime');
+        $qb->addSelect('sum(d.dealerTip)');
+        $qb->from('Solcre\Pokerclub\Entity\DealerTipSessionEntity', 'd');
+        $qb->join('d.session', 's');
+        $qb->groupBy('s.id');
+        $qb->where('s.startTime BETWEEN :from AND :to');
+        $qb->setParameter('from', $from);
+        $qb->setParameter('to', $to);
+
+        return $qb->getQuery()->getResult();
+    }
+
+    public function fetchServiceTipsBetweenDates(\DateTime $from, \DateTime $to)
+    {
+        $qb = $this->_em->createQueryBuilder();
+        $qb->select('s.id');
+        $qb->addSelect('s.startTime');
+        $qb->addSelect('sum(st.serviceTip)');
+        $qb->from('Solcre\Pokerclub\Entity\ServiceTipSessionEntity', 'st');
+        $qb->join('st.session', 's');
+        $qb->groupBy('s.id');
+        $qb->where('s.startTime BETWEEN :from AND :to');
+        $qb->setParameter('from', $from);
+        $qb->setParameter('to', $to);
+
+        return $qb->getQuery()->getResult();
+    }
+
+    public function fetchExpensesBetweenDates(\DateTime $from, \DateTime $to)
+    {
+        $qb = $this->_em->createQueryBuilder();
+        $qb->select('s.id');
+        $qb->addSelect('s.startTime');
+        $qb->addSelect('sum(e.amount)');
+        $qb->from('Solcre\Pokerclub\Entity\ExpensesSessionEntity', 'e');
+        $qb->join('e.session', 's');
+        $qb->groupBy('s.id');
+        $qb->where('s.startTime BETWEEN :from AND :to');
+        $qb->setParameter('from', $from);
+        $qb->setParameter('to', $to);
+
+        return $qb->getQuery()->getResult();
+    }
+
+    public function fetchTotalCashinBySession(\DateTime $from, \DateTime $to)
+    {
+        /*
+        $qb = $this->_em->createQueryBuilder();
+        $qb->select('s.id');
+        $qb->addSelect('s.startTime');
+        $qb->addSelect('sum(e.amount)');
+        $qb->from('Solcre\Pokerclub\Entity\ExpensesSessionEntity', 'e');
+        $qb->join('e.session', 's');
+        $qb->groupBy('s.id');
+        $qb->where('s.startTime BETWEEN :from AND :to');
+        $qb->setParameter('from', $from);
+        $qb->setParameter('to', $to);
+
+        return $qb->getQuery()->getResult();
+        */
+
+        return ['result'];
     }
 }
